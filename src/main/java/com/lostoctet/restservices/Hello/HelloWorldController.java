1b22450d8bfe88113f9ef2b1de00a73700ba7a75
Lost Octet <lost.octet@gmail.com>
2022-01-14 00:15:09 +0530

    I18n First Commit

--- src/main/java/com/lostoctet/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/lostoctet/restservices/Hello/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.lostoctet.restservices.Hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
-	
+
+
+	@Autowired
+	ResourceBundleMessageSource messageSource;
+
 	//Simple Method, URI= /helloworld, HTTP method= GET
 	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
 	@GetMapping("/helloworld1")
@@ -18,6 +28,17 @@ public class HelloWorldController {
 	@GetMapping("/hello-bean")
 	public UserDetails helloWorldBean() {
 		return new UserDetails ("Atul", "Pandey", "Faizabad");
+	}
+
+	//Method for I18n i.e. Internationalization
+	@GetMapping("/hello-int")
+	public String getMessagesI18n(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+
+	@GetMapping("/hello-int2")
+	public String getMessagesI18n2() {
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
 	}
 	
 }
--- src/main/java/com/lostoctet/restservices/SpringbootDemoApplication.java
+++ src/main/java/com/lostoctet/restservices/SpringbootDemoApplication.java
@@ -1,13 +1,36 @@
 package com.lostoctet.restservices;
 
+//import jdk.nashorn.internal.ir.LexicalContextNode;
+//import org.hibernate.validator.spi.messageinterpolation.LocaleResolverContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
 //
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
